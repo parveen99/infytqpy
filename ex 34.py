ea2d7e9a6f6875988249db24cf98df3dd3af9cd3
parveen99 <40237237+parveen99@users.noreply.github.com>
2019-06-04 13:03:59 +0530

    Create ex 34.py

---
+++ ex 34.py
@@ -0,0 +1,13 @@
+#PF-Exer-34
+import math
+def find_number_of_combination(number_of_flavours):
+    nmber_of_combination=0
+    nmber_of_combination=2**number_of_flavours
+    
+    
+    return nmber_of_combination
+
+
+#Provide different values for number_of_flavours and test your program
+number_of_combination=find_number_of_combination(5)
+print(number_of_combination)
